de6856524da2b79b7f4d9e9ada7a0c29502acad7
Chris Roblee <croblee@trustar.co>
2016-08-10 11:19:49 -0700

    Include report timestamps, limit number of reports.

--- examples/ingest_csv.py
+++ examples/ingest_csv.py
@@ -4,7 +4,7 @@
 Converts each row in a CSV file into an incident report and submits to TruSTAR.
 
 EXAMPLE:
-python ingest_csv.py -c "TargetIP,SourceIP,Info,Analysis,Indicators" -t "TrackingNumber" -f  august_incident_report.csv
+python ingest_csv.py -c "TargetIP,SourceIP,Info,Analysis,Indicators" -t "TrackingNumber" -d "ReportTime" -f  august_incident_report.csv
 """
 
 import argparse
@@ -25,8 +25,13 @@ def main():
                                                   'python ingest_csv.py -c "TargetIP,SourceIP,Info,Analysis,Indicators" -t "TrackingNumber" -f  august_incident_report.csv'))
     parser.add_argument('-f', '--file', required=True, dest='file_name', help='csv file to import')
     parser.add_argument('-t', '--title', required=True, dest='title_col', help='Name of column to use as title field')
+    parser.add_argument('-d', '--datetime', required=False, dest='datetime_col',
+                        help='Name of column to use as datetime field')
     parser.add_argument('-c', '--columns', required=False, dest='cols',
                         help='List of comma-separated column names to include')
+    parser.add_argument('-n', '--num-reports', required=False, dest='num_reports', type=int, default=1000,
+                        help='Max number of reports to submit (top-down order)')
+
     args = parser.parse_args()
 
     allowed_keys_content = []
@@ -35,10 +40,10 @@ def main():
         allowed_keys_content = args.cols.split(",")
 
     # noinspection PyCallingNonCallable
-    ts = TruStar()
+    ts = TruStar(config_role="trustar")
     token = ts.get_token()
 
-    df = pd.read_csv(args.file_name)
+    df = pd.read_csv(args.file_name, nrows=args.num_reports)
 
     # Create title and report content from the provided column names (if any)
     all_reports = []
@@ -46,6 +51,7 @@ def main():
     for report_num in xrange(0, len(df)):
         current_content = ''
         current_title = ''
+        current_datetime = None
         current_report = {}
         for key in df:
             content = "{}:\n {}\n ".format(key, str(df[key][report_num]))
@@ -54,19 +60,24 @@ def main():
                 current_content += content
             if key == args.title_col:
                 current_title = str(df[key][report_num])
+            if key == args.datetime_col:
+                current_datetime = str(df[key][report_num])
 
         current_report['reportTitle'] = current_title
+        current_report['reportDateTime'] = current_datetime
         current_report['reportContent'] = current_content
         all_reports.append(current_report)
 
     if do_enclave_submissions:
         num_submitted = 0
         for staged_report in all_reports:
+
             successful = False
             attempts = 0
             while not successful and attempts < 5:
                 attempts += 1
                 response = ts.submit_report(token, staged_report['reportContent'], staged_report['reportTitle'],
+                                            discovered_time_str=staged_report['reportDateTime'],
                                             enclave=True)
                 if 'error' in response:
                     print "Submission failed with error: {}, {}".format(response['error'], response['message'])
