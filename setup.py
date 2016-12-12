d38b686574962e4d715739d888a52b7543d2bb12
Chris Roblee <croblee@trustar.co>
2016-12-12 15:38:57 -0800

    Bump version

--- setup.py
+++ setup.py
@@ -4,13 +4,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='trustar',
-    version='0.0.3',
+    version='0.0.4',
     author='TruSTAR Technology Inc.',
     author_email='support@trustar.co',
     url='https://github.com/trustar/trustar-python',
     download_url = 'https://github.com/trustar/trustar-python/tarball/0.0.3',
     description='Python SDK for the TruSTAR REST API',
-    author_email='support@trustar.co',
     license='MIT',
     install_requires=['future',
                       'python-dateutil',
