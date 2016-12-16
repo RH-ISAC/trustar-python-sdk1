df15e5d5a8b051a40abeca64c35abc5a037454b6
Chris Roblee <croblee@trustar.co>
2016-12-15 16:08:52 -0800

    Track submission history in local log file

--- examples/bulk_upload.py
+++ examples/bulk_upload.py
@@ -15,48 +15,64 @@ from trustar import TruStar
 def main():
     parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter,
                                      description=(
-                                         'Submit one or more reports from local files (txt, pdf, docx, etc) in a directory\n\n'
+                                         'Submit one or more reports from local files (txt, pdf, docx, etc) '
+                                         'in a directory\n\n'
                                          'Example:\n'
                                          'python bulk_upload.py ./sample_reports'))
     parser.add_argument('dir', help='Path containing report files')
+    parser.add_argument('-i', '--ignore', dest='ignore', action='store_true',
+                        help='Ignore history and resubmit already procesed files')
 
     args = parser.parse_args()
     source_report_dir = args.dir
 
     ts = TruStar(config_role="trustar")
     token = ts.get_token()
+
     # process all files in directory
     print("Processing and submitting each source file in %s as a TruSTAR Incident Report" % source_report_dir)
 
-    for (dirpath, dirnames, filenames) in os.walk(source_report_dir):
-        for source_file in filenames:
-            print("Processing source file %s " % source_file)
-            try:
-                path = os.path.join(source_report_dir, source_file)
-                report_body_txt = ts.process_file(path)
+    processed_files = set()
 
-                # response_json = ts.submit_report(token, report_body_txt, "COMMUNITY: " + file)
-                response_json = ts.submit_report(token, report_body_txt, "ENCLAVE: " + source_file, enclave=True)
-                report_id = response_json['reportId']
+    processed_files_file = os.path.join(source_report_dir, "processed_files.log")
+    if os.path.isfile(processed_files_file) and not args.ignore:
+        processed_files = set(line.strip() for line in open(processed_files_file))
 
-                print("SUCCESSFULLY SUBMITTED REPORT, TRUSTAR REPORT as Incident Report ID %s" % report_id)
+    with open(processed_files_file, 'a', 0) as pf:
+        for (dirpath, dirnames, filenames) in os.walk(source_report_dir):
+            for source_file in filenames:
+                if source_file in processed_files:
+                    continue
 
-                # if 'reportIndicators' in response_json:
-                #     print("Extracted the following indicators: {}".format(response_json['reportIndicators']))
-                # else:
-                #     print("No indicators returned from  report id {0}".format(report_id))
-                #
-                # # if 'correlatedIndicators' in response_json:
-                #     print(
-                #         "Extracted the following correlated indicators: {}".format(
-                #             response_json['correlatedIndicators']))
-                # else:
-                #     print("No correlatedIndicators found in report id {0}".format(report_id))
-            except Exception as e:
-                print("Problem with file %s, exception: %s " % (source_file, e))
-                continue
+                print("Processing source file %s " % source_file)
+                try:
+                    path = os.path.join(source_report_dir, source_file)
+                    report_body_txt = ts.process_file(path)
 
-            time.sleep(2)
+                    # response_json = ts.submit_report(token, report_body_txt, "COMMUNITY: " + file)
+                    response_json = ts.submit_report(token, report_body_txt, "ENCLAVE: " + source_file, enclave=True)
+                    report_id = response_json['reportId']
+
+                    print("SUCCESSFULLY SUBMITTED REPORT, TRUSTAR REPORT as Incident Report ID %s" % report_id)
+                    pf.write("%s\n" % source_file)
+
+                    # if 'reportIndicators' in response_json:
+                    #     print("Extracted the following indicators: {}".format(response_json['reportIndicators']))
+                    # else:
+                    #     print("No indicators returned from  report id {0}".format(report_id))
+                    #
+                    # # if 'correlatedIndicators' in response_json:
+                    #     print(
+                    #         "Extracted the following correlated indicators: {}".format(
+                    #             response_json['correlatedIndicators']))
+                    # else:
+                    #     print("No correlatedIndicators found in report id {0}".format(report_id))
+
+                except Exception as e:
+                    print("Problem with file %s, exception: %s " % (source_file, e))
+                    continue
+
+                time.sleep(2)
 
 
 if __name__ == '__main__':
