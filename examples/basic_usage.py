d63136b9690445e563b23b0b4e6f5c6202de9d70
Anastasia Radchenko <aradchenko@trustar.co>
2017-02-24 19:24:11 -0800

    Convert timeBegan to iso8601 format with timezone
    https://trello.com/c/cnxJDtqF/1323-1-ir-began-timestamp-wrong-for-api-submissions

--- examples/basic_usage.py
+++ examples/basic_usage.py
@@ -8,6 +8,11 @@ from __future__ import print_function
 
 import json
 
+from datetime import datetime
+import dateutil.parser
+import dateutil.tz
+import pytz
+
 from trustar import TruStar
 
 do_latest_reports = True
@@ -16,6 +21,7 @@ do_query_indicators = True
 do_latest_indicators = True
 do_comm_submissions = True
 do_enclave_submissions = True
+do_submit_reports = True
 
 # search_string = "1.2.3.4 8.8.8.8 10.0.2.1 185.19.85.172 art-archiv.ru"
 search_string = "167.114.35.70,103.255.61.39,miel-maroc.com"
@@ -33,7 +39,6 @@ def main():
 
         for result in results:
             print("\t{}, {}, {}".format(result['id'], result['distributionType'], result['title']))
-            print(json.dumps(result))
         print()
 
     if do_correlated:
@@ -89,6 +94,47 @@ def main():
 
         if 'reportIndicators' in enclave_response:
             print("Extracted the following enclave indicators: {}".format(enclave_response['reportIndicators']))
+
+    # Submit simple latest 5 community reports with diff format beganTime timestamps
+    if do_submit_reports:
+        print("Testing Different Report Format Submissions")
+
+        ts_src = TruStar(config_role="production")
+        token_src = ts_src.get_token()
+        results = ts_src.get_latest_reports(token_src)
+
+        for result in results:
+            if len(result['indicators']) <= 150:
+                break;
+
+        i = 1
+
+        # sample timestamp used for testing: 1487890914000
+        timestamps = {1487890914000, "2017-02-23T23:01:54", "2017-02-23T23:01:54+0000", dateutil.parser.parse("2017-02-23T23:01:54"), dateutil.parser.parse("2017-02-23T23:01:54+0000")}
+        for timestamp in timestamps:
+            print("~~~Submission " + str(i) + "~~~")
+            print("Timestamp (original): " + str(timestamp))
+            if isinstance(timestamp, int):
+                print("Timestamp type: int")
+            elif isinstance(timestamp, str):
+                print("Timestamp type: string")
+            elif isinstance(timestamp, datetime):
+                print("Timestamp type: datetime")
+            else:
+                print("Timestamp type: unknown")
+            test_tstamp_submission(ts, token, ts_src, result, timestamp)
+            i = i+1
+
+
+def test_tstamp_submission(ts, token, ts_src, result, tstamp):
+    community_response = ts.submit_report(token, result['reportBody'], result['title'], tstamp)
+    print("~~Original Report~~")
+    print("SRC Report ID: %s" % (result['id']))
+    print("Distribution Type: %s" % (result['distributionType']))
+    print("->Time Began (converted): %s" % (ts_src.normalize_timestamp(result['timeBegan'])))
+    print("~~New Report~~")
+    print("DEST Report ID: %s" % (community_response['reportId']))
+    print()
 
 
 if __name__ == '__main__':
