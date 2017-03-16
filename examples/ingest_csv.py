35defaa106c9dd28342d7519c70cfd69c8f3c748
Chris Roblee <croblee@trustar.co>
2017-03-16 16:06:13 -0700

    Fix dateutil, string and timestamp issues

--- examples/ingest_csv.py
+++ examples/ingest_csv.py
@@ -9,7 +9,9 @@ from __future__ import print_function
 
 import argparse
 import json
+import sys
 import time
+import traceback
 
 import cef
 
@@ -21,11 +23,8 @@ from cef import log_cef
 
 import pandas as pd
 from builtins import range
-from builtins import str
-from builtins import bytes
 
 from trustar import TruStar
-import numpy as np
 
 # Set to false to submit to community
 do_enclave_submissions = True
@@ -46,9 +45,8 @@ def main():
                         help='Max number of reports to submit (top-down order)')
     parser.add_argument('-o', '--output', required=False, dest='cef_output_file', default='trustar.cef',
                         help='Common Event Format (CEF) output log file, one event is generated per successful submission')
-    parser.add_argument('-cn', '--CaseName', required=True, dest='casename_col',
-                        help='Name of column to use as report case name')
-
+    parser.add_argument('-ci', '--case-id', required=False, dest='caseid_col',
+                        help='Name of column to use as report case ID for CEF export')
     args = parser.parse_args()
 
     allowed_keys_content = []
@@ -68,8 +66,9 @@ def main():
         current_content = ''
         current_title = ''
         current_datetime = None
-        current_case = {}
+        current_case_id = 0
         current_report = {}
+
         for key in df:
             # ignore empty cells, which are float64 NaNs
             cell_value = df[key][report_num]
@@ -81,7 +80,6 @@ def main():
 
             # encode any unicode chars
             string_value = cell_value.encode('utf-8').strip()
-
             if string_value == "nan":
                 print("%s -> %s" % (key, string_value))
                 continue
@@ -94,13 +92,14 @@ def main():
                 current_title = str(df[key][report_num])
             if key == args.datetime_col:
                 current_datetime = str(df[key][report_num])
-            if key == args.casename_col:
-                current_case = str(df[key][report_num])
+            if key == args.caseid_col:
+                current_case_id = str(df[key][report_num])
 
         current_report['reportTitle'] = current_title
         current_report['reportDateTime'] = current_datetime
         current_report['reportContent'] = current_content
-        current_report['reportCase'] = current_case
+        current_report['reportCaseId'] = current_case_id
+
         all_reports.append(current_report)
 
     if do_enclave_submissions:
@@ -112,10 +111,9 @@ def main():
             while not successful and attempts < 5:
                 attempts += 1
                 try:
-                    response = ts.submit_report(token, staged_report['reportContent'], staged_report['reportTitle'],
-                                                began_time_str=staged_report['reportDateTime'],
-                                                enclave=True)
-                    # noinspection PyPep8
+                    response = ts.submit_report(token, report_body_txt=staged_report['reportContent'],
+                                                report_name=staged_report['reportTitle'],
+                                                began_time=staged_report['reportDateTime'], enclave=True)
                     if 'error' in response:
                         print("Submission failed with error: %s, %s" % (response['error'], response['message']))
                         if response['error'] in (
@@ -128,45 +126,38 @@ def main():
                         num_submitted += 1
                         successful = True
 
-                        print("Submitted report #%s-%s title %s as TruSTAR IR %s with case name: %s" % (num_submitted, attempts,
-                                                                                         staged_report['reportTitle'],
-                                                                                         response['reportId'],
-                                                                                         staged_report['reportCase']))
-
+                        print("Submitted report #%s-%s title %s as TruSTAR IR %s with case ID: %s" % (
+                            num_submitted, attempts,
+                            staged_report['reportTitle'],
+                            response['reportId'],
+                            staged_report['reportCaseId']))
 
                         print("URL: %s" % ts.get_report_url(response['reportId']))
 
-                        # HTTP_USER_AGENT is the cs1 field
-                        # example CEF output: CEF:version|vendor|product|device_version|signature|name|severity|cs1=(num_submitted) cs2=(report_url)
-
+                        # Build CEF output:
+                        # - HTTP_USER_AGENT is the cs1 field
+                        # - example CEF output: CEF:version|vendor|product|device_version|signature|name|severity|cs1=(num_submitted) cs2=(report_url)
                         config = {'cef.version': '0.5', 'cef.vendor': 'TruSTAR',
                                   'cef.device_version': '2.0', 'cef.product': 'API',
                                   'cef': True, 'cef.file': args.cef_output_file}
+
                         environ = {'REMOTE_ADDR': '127.0.0.1', 'HTTP_HOST': '127.0.0.1',
-                                   #'HTTP_USER_AGENT': staged_report['reportCase'].decode(encoding='UTF-8')} --gives me the decode error
-                                   'HTTP_USER_AGENT': staged_report['reportCase']}
+                                   'HTTP_USER_AGENT': staged_report['reportTitle']}
 
                         log_cef('SUBMISSION', 1, environ, config, signature="INFO",
-                                cs2=num_submitted,
+                                cs2=staged_report['reportCaseId'],
                                 cs3=ts.get_report_url(response['reportId']))
 
-                        #Static CEF message - alternative solution
-                        # CEFMessage ="CEF:0.5|TruSTAR|API|2.0|INFO|SUBMISSION|1|cs1=%s cs2=%s cs3=%s"
-                        #%(staged_report['reportCase'],num_submitted,ts.get_report_url(response['reportId']))
-
-                        # CHANGE CEF OUTPUT FILE LOCATION HERE:
-                        # cef_file = open('CEFoutput.cef','a')
-                        # cef_file.write("\n" + CEFMessage + "\n")
-                        # cef_file.close()
-
                         ####
-                        #TODO: ADD YOUR CUSTOM POST-PROCESSING CODE FOR THIS SUBMISSION HERE
+                        # TODO: ADD YOUR CUSTOM POST-PROCESSING CODE FOR THIS SUBMISSION HERE
                         ####
 
-                    if 'reportIndicators' in response and len(response['reportIndicators']) > 0:
-                        print("Indicators:\n %s" % (json.dumps(response['reportIndicators'])))
-                    print()
+                        if 'reportIndicators' in response and len(response['reportIndicators']) > 0:
+                            print("Indicators:\n %s" % (json.dumps(response['reportIndicators'])))
+                        print()
+
                 except Exception as e:
+                    traceback.print_exc(file=sys.stdout)
                     print("Problem submitting report: %s" % e)
                     time.sleep(5)
 
