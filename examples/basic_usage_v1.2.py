09f603c69c068c07902e635ffb6591ad1951d439
Anastasia Radchenko <aradchenko@trustar.co>
2017-06-08 12:09:17 -0700

    Change test cases for basic usage v1.2

--- examples/basic_usage_v1.2.py
+++ examples/basic_usage_v1.2.py
@@ -9,34 +9,34 @@ from trustar import TruStar
 import json
 from random import randint
 
-do_submit_report = False
-do_report_details_by_ext_id = False
-do_update_report_by_ext_id = False
-do_report_details_by_guid = False
-do_update_report_by_guid = False
-do_release_report = False
-do_report_details3 = False
-do_delete_report = True
+do_submit_report = True
+do_report_details_by_ext_id = True
+do_update_report_by_ext_id = True
+do_report_details_by_guid = True
+do_update_report_by_guid = True
+do_release_report_by_ext_id = True
+do_report_details_by_ext_id_2 = True
+do_delete_report_by_ext_id = True
 
-search_string = "167.114.35.70,103.255.61.39,miel-maroc.com,malware.exe"
 submit_indicators = "google.com malware.exe 103.255.61.39"
 
 verify = True
 
 def main():
-    ts = TruStar(config_role="demo")
+    ts = TruStar(config_role="trustar")
     token = ts.get_token(verify=verify)
 
     # generate random id to use as external_id
-    # external_id = str(randint(1, 100))
+    external_id = str(randint(1, 100))
+
     # or use a specific external_id
-    external_id = "321"
+    # external_id = "321"
     report_guid = None
 
     # Submit a test report and retrieve it
     if do_submit_report:
         print("Submit Report")
-        submission_response = ts.submit_report_v12(token, submit_indicators, "CC REPORT A", external_id=external_id,
+        submission_response = ts.submit_report_v12(token, submit_indicators, "Sample SDK Test Report", external_id=external_id,
                                                    began_time="2017-02-01T01:23:45", enclave=True, verify=verify)
 
         print("Report Submitted")
@@ -47,9 +47,9 @@ def main():
     # Get test report previously submitted
     if do_report_details_by_ext_id:
         print("Get Report")
-        result = ts.get_report_details_v12(token, "1234", id_type="external", verify=verify)
+        result = ts.get_report_details_v12(token, external_id, id_type="external", verify=verify)
 
-        print("Report Details")
+        print("Report Details using External ID")
         print("\ttitle: %s" % result['title'])
         print("\texternalTrackingId: %s" % result['externalTrackingId'])
         print("\tindicators: %s" % result['indicators'])
@@ -60,10 +60,10 @@ def main():
     if do_update_report_by_ext_id:
         print("Update Report")
         title = "NEW CC REPORT"
-        body = "updated report body - yahoo.com"
+        body = "updated report body: 21.22.23.24"
         update_response = ts.update_report(token, external_id, id_type="external", title=title, report_body=body, verify=verify)
 
-        print("Updated Report")
+        print("Updated Report using External ID")
         print("\texternalTrackingId: %s" % update_response['externalTrackingId'])
         print("\tindicators: %s" % update_response['reportIndicators'])
         print("\tURL: %s\n" % ts.get_report_url(update_response['reportId']))
@@ -73,7 +73,7 @@ def main():
         print("Get Report")
         result = ts.get_report_details_v12(token, report_guid, id_type="internal", verify=verify)
 
-        print("Report Details")
+        print("Report Details using Guid")
         print("\ttitle: %s" % result['title'])
         print("\texternalTrackingId: %s" % result['externalTrackingId'])
         print("\tindicators: %s" % result['indicators'])
@@ -83,10 +83,10 @@ def main():
     if do_update_report_by_guid:
         print("Update Report")
         title = "New Sample Title"
-        body = "new sample body - google.com"
+        body = "new sample body - 7.8.9.10"
         update_response = ts.update_report(token, report_guid, id_type="internal", title=title, report_body=body, verify=verify)
 
-        print("Updated Report by Guid")
+        print("Updated Report using GUID")
         print("\texternalTrackingId: %s" % update_response['externalTrackingId'])
         print("\tindicators: %s" % update_response['reportIndicators'])
         print("\tURL: %s\n" % ts.get_report_url(update_response['reportId']))
@@ -96,38 +96,38 @@ def main():
         print("Get Report")
         result = ts.get_report_details_v12(token, report_guid, id_type="internal", verify=verify)
 
-        print("Report Details")
+        print("Report Details using GUID")
         print("\ttitle: %s" % result['title'])
         print("\texternalTrackingId: %s" % result['externalTrackingId'])
         print("\tindicators: %s" % result['indicators'])
         print("\tURL: %s\n" % ts.get_report_url(result['id']))
 
     # Release report to community
-    if do_release_report:
+    if do_release_report_by_ext_id:
         print("Release Report")
         update_response = ts.update_report(token, external_id, id_type='external', distribution="COMMUNITY", verify=verify)
 
-        print("Report Released")
+        print("Report Released using External ID")
         print("\texternalTrackingId: %s" % update_response['externalTrackingId'])
         print("\tindicators: %s" % update_response['reportIndicators'])
         print("\tURL: %s\n" % ts.get_report_url(update_response['reportId']))
 
     # Get test report previously submitted
-    if do_report_details3:
+    if do_report_details_by_ext_id_2:
         print("Get Report")
         result = ts.get_report_details_v12(token, external_id, id_type="external", verify=verify)
 
-        print("Report Details")
+        print("Report Details using External ID")
         print("\ttitle: %s" % result['title'])
         print("\texternalTrackingId: %s" % result['externalTrackingId'])
         print("\tindicators: %s" % result['indicators'])
         print("\tURL: %s\n" % ts.get_report_url(result['id']))
 
     # Delete test report previously submitted
-    if do_delete_report:
+    if do_delete_report_by_ext_id:
         print("Delete Report")
-        response = ts.delete_report(token, "1", id_type="external", verify=verify)
-        print("Report Deleted")
+        response = ts.delete_report(token, external_id, id_type="external", verify=verify)
+        print("Report Deleted using External ID")
 
 
 if __name__ == '__main__':
