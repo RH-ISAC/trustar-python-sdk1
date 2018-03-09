781e5b97fd1a1b94bd7677b4e87eb3e5fb589a1e
mknopf <mknopf@trustar.co>
2018-03-09 13:26:16 -0800

    remove slice

--- trustar/models/__init__.py
+++ trustar/models/__init__.py
@@ -1,7 +1,6 @@
 from .enclave import Enclave
 from .indicator import Indicator
 from .page import Page
-from .slice import Slice
 from .report import Report
 from .tag import Tag
 from .enum import *
