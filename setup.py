8a5b98161194ffc736857bbdb72f7447aa54c510
mknopf <mknopf@trustar.co>
2018-09-10 15:11:32 -0700

    use page number and size parameters

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-# git tag 0.3.13.4 -m "Bump version" && git push --tags origin master
+# git tag 0.3.13.5 -m "Bump version" && git push --tags origin master
 # python setup.py sdist
 # twine upload --skip-existing dist/*
 
