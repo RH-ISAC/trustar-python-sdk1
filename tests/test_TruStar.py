b16bf459b6477fd96360f777e3aa588cdbe0f020
mknopf <mknopf@trustar.co>
2018-10-25 15:58:55 -0700

    do not submit report with no enclaves in test

--- tests/test_TruStar.py
+++ tests/test_TruStar.py
@@ -62,7 +62,6 @@ class TruStarTests(unittest.TestCase):
 
         # update report
         report.body = "Bleh bleh bleh"
-        report.enclave_ids = None
         self.ts.update_report(report=report)
 
         # get report
