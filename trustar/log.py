714ba612d5e17a095a993d4bdf72a817663c631c
YaPeL <elmaildejapel@gmail.com>
2020-08-21 19:08:48 -0300

    nested fields on our json messages (#80)

--- trustar/log.py
+++ trustar/log.py
@@ -1,5 +1,5 @@
 # Seems like from time to time people do a from trustar import *, lets minimize the chances of something ugly happening
-__all__ = ["get_logger"]
+__all__ = ["get_logger", "TrustarJSONFormatter"]
 
 import datetime
 import logging
@@ -15,6 +15,23 @@ class TrustarJSONFormatter(json_log_formatter.JSONFormatter):
     """
     Custom class to override the default behaviour of the JSONFormatter
     """
+
+    def format(self, record):
+        """
+        the default behaviour of JSONFormatter is to cast everything into a string
+        we avoid calling getMessage and leave the object as it is, this way our json messages can have nested dicts
+        """
+        message = record.msg
+        extra = self.extra_from_record(record)
+        json_record = self.json_record(message, extra, record)
+        mutated_record = self.mutate_json_record(json_record)
+        # Backwards compatibility: Functions that overwrite this but don't
+        # return a new value will return None because they modified the
+        # argument passed in.
+        if mutated_record is None:
+            mutated_record = json_record
+        return self.to_json(mutated_record)
+
     def json_record(self, message, extra, record):
         extra['message'] = message
         extra['level'] = record.levelname
@@ -23,6 +40,23 @@ class TrustarJSONFormatter(json_log_formatter.JSONFormatter):
         if record.exc_info:
             extra['exec_info'] = self.formatException(record.exc_info)
         return extra
+
+    def to_json(self, record):
+        """Converts record dict to a JSON string.
+
+        It makes best effort to serialize a record (represents an object as a string)
+        instead of raising TypeError if json library supports default argument.
+        Note, ujson doesn't support it.
+        """
+        return self.json_lib.dumps(record, default=_json_object_encoder)
+
+
+def _json_object_encoder(obj):
+    try:
+        return obj.to_json()
+    except AttributeError:
+        return str(obj)
+
 
 
 def get_handler():
